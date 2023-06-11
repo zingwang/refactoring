6513211500e0376484cf8075dc18db78ba3556d1
zingwang <zingwang06@naver.com>
2023-06-11 19:41:48 +0900

    [가변데이터] 리팩토링 22. 여러 함수를 변환 함수로 묶기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
@@ -3,22 +3,14 @@ package me.whiteship.refactoring._06_mutable_data._22_combine_functions_into_tra
 import java.time.Month;
 import java.time.Year;
 
-public class Client2 {
+public class Client2 extends ReadingClinet{
 
     private double base;
     private double taxableCharge;
 
     public Client2(Reading reading) {
         this.base = baseRate(reading.month(), reading.year()) * reading.quantity();
-        this.taxableCharge = Math.max(0, this.base - taxThreshold(reading.year()));
-    }
-
-    private double taxThreshold(Year year) {
-        return 5;
-    }
-
-    private double baseRate(Month month, Year year) {
-        return 10;
+        this.taxableCharge = enrichReading(reading).taxableCharge();
     }
 
     public double getBase() {
