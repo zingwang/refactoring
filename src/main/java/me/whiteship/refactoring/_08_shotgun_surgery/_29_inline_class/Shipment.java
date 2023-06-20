84260c7a0d35b662df8b99b3669b7a639cedc69b
zingwang <zingwang06@naver.com>
2023-06-20 19:23:46 +0900

    [산탄총 수술] 리팩토링 29. 클래스 인라인

--- src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
+++ src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
@@ -1,22 +1,36 @@
 package me.whiteship.refactoring._08_shotgun_surgery._29_inline_class;
 
 public class Shipment {
-
-    private TrackingInformation trackingInformation;
-
-    public Shipment(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
+    public String getShippingCompany() {
+        return shippingCompany;
     }
 
-    public TrackingInformation getTrackingInformation() {
-        return trackingInformation;
+    public void setShippingCompany(String shippingCompany) {
+        this.shippingCompany = shippingCompany;
     }
 
-    public void setTrackingInformation(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
+    public String getTrackingNumber() {
+        return trackingNumber;
     }
 
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    private String shippingCompany;
+
+    private String trackingNumber;
+    public Shipment(String shippingCompany, String trackingNumber) {
+        this.shippingCompany = shippingCompany;
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String display() {
+        return this.shippingCompany + ": " + this.trackingNumber;
+    }
     public String getTrackingInfo() {
-        return this.trackingInformation.display();
+        return this.display();
     }
+
+
 }
