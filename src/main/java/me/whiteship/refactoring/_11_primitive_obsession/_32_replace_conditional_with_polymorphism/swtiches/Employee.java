2b3852f998160726a212523598576e5152dcb330
zingwang <zingwang06@naver.com>
2023-06-26 19:11:31 +0900

    [기본형 집착] 리팩토링 32. 조건부 로직을 다형성으로 바꾸기 : 스위치

--- src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/swtiches/Employee.java
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/swtiches/Employee.java
@@ -4,29 +4,26 @@ import java.util.List;
 
 public class Employee {
 
-    private String type;
+    protected String type;
 
-    private List<String> availableProjects;
+    protected List<String> availableProjects;
 
     public Employee(String type, List<String> availableProjects) {
         this.type = type;
         this.availableProjects = availableProjects;
     }
+    public Employee(List<String> availableProjects) {
+        this.availableProjects = availableProjects;
+    }
+
+    public Employee() {
+    }
 
     public int vacationHours() {
-        return switch (type) {
-            case "full-time" -> 120;
-            case "part-time" -> 80;
-            case "temporal" -> 32;
-            default -> 0;
-        };
+        return 0;
     }
 
     public boolean canAccessTo(String project) {
-        return switch (type) {
-            case "full-time" -> true;
-            case "part-time", "temporal" -> this.availableProjects.contains(project);
-            default -> false;
-        };
+        return false;
     }
 }
