44770539f074cec5b129b2fdea46bb667e54cd4c
zingwang <zingwang06@naver.com>
2023-06-28 22:37:57 +0900

    [반복문] 리팩토링 33.반복문을 파이프라인으로 바꾸기

--- src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
+++ src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -2,6 +2,9 @@ package me.whiteship.refactoring._13_loop._33_replace_loop_with_pipeline;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Author {
 
@@ -15,15 +18,13 @@ public class Author {
     }
 
     static public List<String> TwitterHandles(List<Author> authors, String company) {
-        var result = new ArrayList<String> ();
-        for (Author a : authors) {
-            if (a.company.equals(company)) {
-                var handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
-        return result;
+
+       return  authors.stream()
+               .filter(author -> author.company.equals(company))
+               .map(author -> author.twitterHandle)
+               .filter(Objects::nonNull)
+               .collect(Collectors.toList());
+
     }
 
 }
