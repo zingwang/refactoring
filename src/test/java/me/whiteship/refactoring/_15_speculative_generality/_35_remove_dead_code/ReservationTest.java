13cb75f9228fcbf6730eeb13372b9bacbcb1089c
zingwang <zingwang06@naver.com>
2023-06-30 20:21:53 +0900

    [추측성 일반화] 리팩토링 35. 죽은 코드 제거하기

--- src/test/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/ReservationTest.java
+++ src/test/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/ReservationTest.java
@@ -8,14 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationTest {
 
-    @Test
-    void reservation() {
-        Reservation reservation = new Reservation(
-                "tennis",
-                LocalDateTime.of(2022, 1, 20, 19, 30),
-                LocalDateTime.of(2022, 1, 20, 9, 0));
-        reservation.setAlarmBefore(30);
-        assertEquals(LocalDateTime.of(2022, 1, 20, 19, 0), reservation.getAlarm());
-    }
+
 
 }
